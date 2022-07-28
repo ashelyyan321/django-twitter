f274eac6a12504dd5f3241e7d82bf9f60e293399
Frida Yan <690318347@qq.com>
2022-07-28 16:38:12 +0800

    endlessp

--- tweets/api/tests.py
+++ tweets/api/tests.py
@@ -2,6 +2,7 @@ from rest_framework.test import APIClient
 from testing.testcases import TestCase
 from tweets.models import Tweet, TweetPhoto
 from django.core.files.uploadedfile import SimpleUploadedFile
+from utils.paginations import EndlessPagination
 
 
 # 注意要加 '/' 结尾，要不然会产生 301 redirect
@@ -30,18 +31,18 @@ class TweetApiTests(TestCase):
     def test_list_api(self):
         # 必须带 user_id
         response = self.anonymous_client.get(TWEET_LIST_API)
-        print(response.data, response.status_code)
+        #print(response.data, response.status_code)
         self.assertEqual(response.status_code, 400)
 
         # 正常 request
         response = self.anonymous_client.get(TWEET_LIST_API, {'user_id': self.user1.id})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data['tweets']), 3)
+        self.assertEqual(len(response.data['results']), 3)
         response = self.anonymous_client.get(TWEET_LIST_API, {'user_id': self.user2.id})
-        self.assertEqual(len(response.data['tweets']), 2)
+        self.assertEqual(len(response.data['results']), 2)
         # 检测排序是按照新创建的在前面的顺序来的
-        self.assertEqual(response.data['tweets'][0]['id'], self.tweets2[1].id)
-        self.assertEqual(response.data['tweets'][1]['id'], self.tweets2[0].id)
+        self.assertEqual(response.data['results'][0]['id'], self.tweets2[1].id)
+        self.assertEqual(response.data['results'][1]['id'], self.tweets2[0].id)
 
     def test_create_api(self):
         # 必须登录
@@ -73,7 +74,7 @@ class TweetApiTests(TestCase):
         # tweet with id=-1 does not exist
         url = TWEET_RETRIEVE_API.format(-1)
         response = self.anonymous_client.get(url)
-        print(response.data, response.status_code)
+        #print(response.data, response.status_code)
         self.assertEqual(response.status_code, 404)
 
         # 获取某个 tweet 的时候会一起把 comments 也拿下
@@ -88,66 +89,113 @@ class TweetApiTests(TestCase):
         response = self.anonymous_client.get(url)
         self.assertEqual(len(response.data['comments']), 2)
 
-        def test_create_with_files(self):
-            # 上传空文件列表
-            response = self.user1_client.post(TWEET_CREATE_API, {
-                'content': 'a selfie',
-                'files': [],
-            })
-            self.assertEqual(response.status_code, 201)
-            self.assertEqual(TweetPhoto.objects.count(), 0)
+    def test_create_with_files(self):
+        # 上传空文件列表
+        response = self.user1_client.post(TWEET_CREATE_API, {
+            'content': 'a selfie',
+            'files': [],
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(TweetPhoto.objects.count(), 0)
 
-            # 上传单个文件
-            # content 需要是一个 bytes 类型，所以用 str.encode 转换一下
-            file = SimpleUploadedFile(
-                name='selfie.jpg',
-                content=str.encode('a fake image'),
+        # 上传单个文件
+        # content 需要是一个 bytes 类型，所以用 str.encode 转换一下
+        file = SimpleUploadedFile(
+            name='selfie.jpg',
+            content=str.encode('a fake image'),
+            content_type='image/jpeg',
+        )
+        response = self.user1_client.post(TWEET_CREATE_API, {
+            'content': 'a selfie',
+            'files': [file],
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(TweetPhoto.objects.count(), 1)
+
+        # 测试多个文件上传
+        file1 = SimpleUploadedFile(
+            name='selfie1.jpg',
+            content=str.encode('selfie 1'),
+            content_type='image/jpeg',
+        )
+        file2 = SimpleUploadedFile(
+            name='selfie2.jpg',
+            content=str.encode('selfie 2'),
+            content_type='image/jpeg',
+        )
+        response = self.user1_client.post(TWEET_CREATE_API, {
+            'content': 'two selfies',
+            'files': [file1, file2],
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(TweetPhoto.objects.count(), 3)
+
+        # 从读取的 API 里确保已经包含了 photo 的地址
+        retrieve_url = TWEET_RETRIEVE_API.format(response.data['id'])
+        response = self.user1_client.get(retrieve_url)
+        self.assertEqual(len(response.data['photo_urls']), 2)
+        self.assertEqual('selfie1' in response.data['photo_urls'][0], True)
+        self.assertEqual('selfie2' in response.data['photo_urls'][1], True)
+
+        # 测试上传超过 9 个文件会失败
+        files = [
+            SimpleUploadedFile(
+                name=f'selfie{i}.jpg',
+                content=str.encode(f'selfie{i}'),
                 content_type='image/jpeg',
             )
-            response = self.user1_client.post(TWEET_CREATE_API, {
-                'content': 'a selfie',
-                'files': [file],
-            })
-            self.assertEqual(response.status_code, 201)
-            self.assertEqual(TweetPhoto.objects.count(), 1)
+            for i in range(10)
+        ]
+        response = self.user1_client.post(TWEET_CREATE_API, {
+            'content': 'failed due to number of photos exceeded limit',
+            'files': files,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(TweetPhoto.objects.count(), 3)
 
-            # 测试多个文件上传
-            file1 = SimpleUploadedFile(
-                name='selfie1.jpg',
-                content=str.encode('selfie 1'),
-                content_type='image/jpeg',
-            )
-            file2 = SimpleUploadedFile(
-                name='selfie2.jpg',
-                content=str.encode('selfie 2'),
-                content_type='image/jpeg',
-            )
-            response = self.user1_client.post(TWEET_CREATE_API, {
-                'content': 'two selfies',
-                'files': [file1, file2],
-            })
-            self.assertEqual(response.status_code, 201)
-            self.assertEqual(TweetPhoto.objects.count(), 3)
+    def test_pagination(self):
+        page_size = EndlessPagination.page_size
 
-            # 从读取的 API 里确保已经包含了 photo 的地址
-            retrieve_url = TWEET_RETRIEVE_API.format(response.data['id'])
-            response = self.user1_client.get(retrieve_url)
-            self.assertEqual(len(response.data['photo_urls']), 2)
-            self.assertEqual('selfie1' in response.data['photo_urls'][0], True)
-            self.assertEqual('selfie2' in response.data['photo_urls'][1], True)
+        # create page_size * 2 tweets
+        # we have created self.tweets1 in setUp
+        for i in range(page_size * 2 - len(self.tweets1)):
+            self.tweets1.append(self.create_tweet(self.user1, 'tweet{}'.format(i)))
 
-            # 测试上传超过 9 个文件会失败
-            files = [
-                SimpleUploadedFile(
-                    name=f'selfie{i}.jpg',
-                    content=str.encode(f'selfie{i}'),
-                    content_type='image/jpeg',
-                )
-                for i in range(10)
-            ]
-            response = self.user1_client.post(TWEET_CREATE_API, {
-                'content': 'failed due to number of photos exceeded limit',
-                'files': files,
-            })
-            self.assertEqual(response.status_code, 400)
-            self.assertEqual(TweetPhoto.objects.count(), 3)
+        tweets = self.tweets1[::-1]
+
+        # pull the first page
+        response = self.user1_client.get(TWEET_LIST_API, {'user_id': self.user1.id})
+        self.assertEqual(response.data['has_next_page'], True)
+        self.assertEqual(len(response.data['results']), page_size)
+        self.assertEqual(response.data['results'][0]['id'], tweets[0].id)
+        self.assertEqual(response.data['results'][1]['id'], tweets[1].id)
+        self.assertEqual(response.data['results'][page_size - 1]['id'], tweets[page_size - 1].id)
+
+        # pull the second page
+        response = self.user1_client.get(TWEET_LIST_API, {
+            'created_at__lt': tweets[page_size - 1].created_at,
+            'user_id': self.user1.id,
+        })
+        self.assertEqual(response.data['has_next_page'], False)
+        self.assertEqual(len(response.data['results']), page_size)
+        self.assertEqual(response.data['results'][0]['id'], tweets[page_size].id)
+        self.assertEqual(response.data['results'][1]['id'], tweets[page_size + 1].id)
+        self.assertEqual(response.data['results'][page_size - 1]['id'], tweets[2 * page_size - 1].id)
+
+        # pull latest newsfeeds
+        response = self.user1_client.get(TWEET_LIST_API, {
+            'created_at__gt': tweets[0].created_at,
+            'user_id': self.user1.id,
+        })
+        self.assertEqual(response.data['has_next_page'], False)
+        self.assertEqual(len(response.data['results']), 0)
+
+        new_tweet = self.create_tweet(self.user1, 'a new tweet comes in')
+
+        response = self.user1_client.get(TWEET_LIST_API, {
+            'created_at__gt': tweets[0].created_at,
+            'user_id': self.user1.id,
+        })
+        self.assertEqual(response.data['has_next_page'], False)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['id'], new_tweet.id)
